ddb8de6834c8f8762960e5009b941f6882431554
mccabmic <mccabmic@oregonstate.edu>
2019-02-13 23:05:25 -0600

    first commit

---
+++ mccabmic.adventure.c
@@ -0,0 +1,220 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <time.h>
+
+#define BUFFER 256
+#define PLAY_ROOMS 7
+
+enum room_type {START_ROOM, MID_ROOM, END_ROOM};
+struct Room {
+	char name[BUFFER], type[BUFFER];
+	char connections[PLAY_ROOMS][BUFFER];
+	int numConnections;
+};
+
+struct Map {
+	struct Room rooms[PLAY_ROOMS];
+	int room_start;
+	int room_end;
+	int game_state;
+	int numRooms;
+};
+
+void init_room(struct Room* room, char* name, char* type, int num_connections, char connections[PLAY_ROOMS][BUFFER]){
+	memset(room->name, '\0', BUFFER);
+	memset(room->type, '\0', BUFFER);
+	strcpy(room->name, name);
+	strcpy(room->type, type);
+	
+	room->numConnections = num_connections;
+	int i;
+	for (i = 0; i < room->numConnections; i++){
+		strcpy(room->connections[i], connections[i]);
+	}
+}
+
+struct Room serialize(char* data_string){
+	FILE *sstream;
+	sstream = fmemopen(data_string, strlen(data_string), "r");
+
+	char* line = NULL;
+	size_t line_len = 0;
+	size_t numCharacters = 0;
+	int line_number = 0;
+
+	int numConnections = 0;
+	char name[BUFFER], type[BUFFER];
+	char myConnections[PLAY_ROOMS][BUFFER];
+	char connectionName[BUFFER];
+	
+	while ((numCharacters = getline(&line, &line_len, sstream)) != -1){
+		if (line_number == 0){
+			sscanf(line, "%*s %*s %s", name);
+		}
+		else{
+			sscanf(line, "%*s %*s %s", myConnections[line_number -1]);
+		}
+		line_number += 1;
+	}
+	
+	sscanf(line, "%*s %*s %s", type);
+	free(line);
+	fclose(sstream);
+	
+	struct Room test_room;
+	init_room(&test_room, name, type, line_number - 2, myConnections);
+	return test_room;
+}
+
+char* read_file(char* filename){
+	FILE *fp = fopen(filename, "r");
+	if (fp < 0){
+		fprintf(stderr, "Failure to open file %s\n", filename);
+		exit(1);
+	}
+	size_t buffer_size;
+	char* file_contents = NULL;
+	size_t numCharacters = getdelim(&file_contents, &buffer_size, '\0', fp);
+	fclose(fp);
+	if (numCharacters != -1){
+		return file_contents;
+	}
+	else {
+		free (file_contents);
+		return 0;
+	}
+}
+int validate_dir(char* directory){
+	char* charPtr = strstr(directory, "mccabmic.rooms.");
+	if (charPtr == 0){
+		return 0;
+	}
+	else{
+		return *charPtr;
+	}
+}
+
+/*https://stackoverflow.com/questions/19117131/get-list-of-file-names-and-store-them-in-array-on-linux-using-c*/
+char* get_most_recent(){
+	DIR* directoryPtr;
+	struct dirent *dir;
+	struct stat dirStat;
+	
+	char* dirName = malloc(sizeof(char)*BUFFER);
+	memset(dirName, '\0', BUFFER);
+	time_t latest = 0;
+	
+	directoryPtr = opendir(".");
+	if (directoryPtr){
+		while ((dir = readdir(directoryPtr)) != NULL){
+			memset(&dirStat, 0, sizeof(dirStat));
+			if (dir->d_type == DT_DIR && strcmp(dir->d_name, ".") && strcmp(dir->d_name, "..")){	
+					stat(dir->d_name, &dirStat);
+					if( dirStat.st_mtime > latest){
+						latest = dirStat.st_mtime;
+						strcpy(dirName, dir->d_name);
+					}
+			}
+		}
+	}
+	closedir(directoryPtr);
+	return dirName;
+
+}
+int list_of_files(char* directory, char myFiles[PLAY_ROOMS][BUFFER]){
+	DIR* directoryPtr;
+	struct dirent *dir;
+	int count = 0;
+	directoryPtr = opendir(directory);
+	if(directoryPtr){
+		while ((dir = readdir(directoryPtr)) != NULL){
+			if (dir->d_type != DT_DIR && strcmp(dir->d_name, ".") && strcmp(dir->d_name, "..")){	
+				strcpy(myFiles[count], dir->d_name);
+				count += 1;
+				}
+			}
+		}	
+	closedir(directoryPtr);
+	return count;
+}
+
+struct Map* generate_map(char* directory){
+	if (validate_dir(directory) == 0){
+		fprintf(stderr, "Error, invalid directory provided to generate_map()\n");
+		exit(1);
+	}
+	
+	struct Map* map = malloc(sizeof(struct Map));
+	char myFiles[PLAY_ROOMS][BUFFER];
+	int count = list_of_files(directory, myFiles);
+	if (count < PLAY_ROOMS){
+		fprintf(stderr, "Error, directory did not contain sufficient number of room files\n");
+		exit(1);
+	};
+	printf("files found: %d\n", count);
+	chdir(directory);
+	int i;
+	for (i = 0; i < PLAY_ROOMS; i++){
+		char* file_contents = read_file(myFiles[i]);
+		struct Room temp_room = serialize(file_contents);
+
+		memcpy(&map->rooms[i], &temp_room, sizeof(temp_room));
+		if(strcmp(map->rooms[i].type, "START_ROOM") == 0){
+			map->room_start = i;
+			}
+		else if (strcmp(map->rooms[i].type, "END_ROOM") == 0){
+			map->room_end = i;
+		}
+		free(file_contents);
+	}
+	map->numRooms = PLAY_ROOMS;
+	map->game_state = 1;
+	return map;
+}
+
+void game_loop(struct Map* map){
+	int player = map->room_start;
+	int goal_room = map->room_end;
+	printf("cheating: start is %d\n", player);
+	printf("cheating: end ins %d\n", goal_room);
+
+	while(map->game_state == 1){
+		printf("CURRENT LOCATION: %s\n", map->rooms[player].name);
+		printf("POSSIBLE CONNECTIONS:");
+		int i;
+		for (i = 0; i < map->rooms[player].numConnections - 1; i++){
+			printf(" %s,", map->rooms[player].connections[i]);
+		}
+		printf(" %s.\n", map->rooms[player].connections[i]);
+		printf("WHERE TO? >");
+
+		if (goal_room < player){
+			player -= 1;
+		}
+		else{
+			player += 1;
+		}
+		printf("\n");
+		if (player == goal_room){
+			map->game_state = 0;
+		}		
+	}
+
+	printf("oh shit, you won!\n");
+}
+int main(){
+	char* my_dir = get_most_recent();
+	struct Map* my_map = generate_map(my_dir);
+	game_loop(my_map);
+	
+	free(my_dir);
+	free(my_map);
+	
+	return 0;
+	
+}
---
+++ mccabmic.buildrooms.c
@@ -0,0 +1,206 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h> /*mkdir chdir*/
+#include <time.h> /*seed */
+#include <string.h> /*memcopy*/
+#include <unistd.h> /*pid*/
+
+#define MAX_ROOMS 10
+#define PLAY_ROOMS 7
+
+/* ENUMS AND DATA STRUCTURES */
+enum room_type {START_ROOM, MID_ROOM, END_ROOM};
+const char* room_names[MAX_ROOMS] = {
+	"Lakeview",
+	"Wrigleyville",
+	"Edgewater",
+	"OldTown",
+	"Loop",
+	"SLoop",
+	"Lincoln_Park",
+	"Wicker_Park",
+	"Pilsen",
+	"Chinatown"
+};
+
+struct Room {
+	int id;
+	const char* name;
+	enum room_type type;
+	int numConnections;
+	int neighbors[PLAY_ROOMS];
+};
+
+/* UTILITY AND DEBUGGING FUNCTIONS */
+void swap(int *a, int *b){
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void print_array(int *array, int n){
+	int i;
+	for (i = 0; i < n; i++){
+		printf("%d ", array[i]);
+	}
+	printf("\n");
+}
+
+void print_room(struct Room printRoom){
+	printf("room id: %d\n", printRoom.id);
+	printf("room name: %s\n", printRoom.name);
+	printf("room type: %d\n", printRoom.type);
+	printf("room connections: %d\n", printRoom.numConnections);
+	printf("room neighbors: ");
+	int i;
+	for (i = 0; i < PLAY_ROOMS; i++){
+		printf( "%d ", printRoom.neighbors[i]); 
+	}
+	printf("\n");
+}
+
+void shuffle(int *array, int n){
+	int i;
+	int j = 0;
+	for (i = n - 1; i >= 1; i--){
+		j = rand() % (i + 1);
+		swap(&array[i],&array[j]);
+	}
+}
+
+/* CORE FUNCTIONALITY */
+void init_rooms(struct Room rooms[PLAY_ROOMS]){
+	int random_order[PLAY_ROOMS];
+	int i;
+	for (i = 0; i < PLAY_ROOMS; i++){
+		random_order[i] = i;
+	}
+
+	shuffle(random_order, PLAY_ROOMS);
+	int default_connections[PLAY_ROOMS]= {0};
+	for (i = 0; i < PLAY_ROOMS; i++){
+		rooms[i].id = i;
+		rooms[i].name = room_names[random_order[i]];
+		rooms[i].type = MID_ROOM;
+		rooms[i].numConnections = 0;
+		memcpy(rooms[i].neighbors, default_connections, sizeof(rooms[i].neighbors));
+	}
+	rooms[0].type = START_ROOM;
+	rooms[PLAY_ROOMS-1].type = END_ROOM;
+}
+
+char* createDir(){
+	int id = getpid();
+	char* directory = malloc(30);
+	sprintf(directory, "%s.%d", "mccabmic.rooms", id);
+	if (mkdir(directory, 0755) == -1){
+		fprintf(stderr, "%s", "Error, directory failed to create\n");
+		return 0;
+		}
+
+	return directory;
+}
+
+/* GRAPH FUNCTIONS */
+int isGraphFull(struct Room rooms[PLAY_ROOMS]){
+	int full_rooms = 0;
+	int i;
+	for (i = 0; i < PLAY_ROOMS; i++){
+		if (rooms[i].numConnections >= 3 && rooms[i].numConnections < 6){
+			full_rooms += 1;
+		}
+	}
+	return (full_rooms == PLAY_ROOMS - 1);
+}
+
+int CanAddConnectionFrom(struct Room* xPtr){
+	return (xPtr->numConnections < 6);
+}
+
+int ConnectionAlreadyExists(struct Room* xPtr, struct Room* yPtr){
+	return (xPtr->neighbors[yPtr->id] == yPtr->neighbors[xPtr->id] && xPtr->neighbors[yPtr->id] == 1) ;
+}
+
+void ConnectRoom(struct Room* xPtr, struct Room* yPtr){
+	xPtr->neighbors[yPtr->id] = 1;
+	xPtr->numConnections += 1;
+}
+
+int isSameRoom (struct Room* xPtr, struct Room* yPtr){
+	return (xPtr->id == yPtr->id);
+}
+
+void AddRandomConnection(struct Room rooms[PLAY_ROOMS]){
+	struct Room* randomRoom;
+	struct Room* otherRoom;
+
+	while(1){
+		randomRoom = &rooms[rand() % PLAY_ROOMS];
+		if (CanAddConnectionFrom(randomRoom) == 1){
+			break;
+		}
+	}
+
+	do{
+		otherRoom = &rooms[rand() % PLAY_ROOMS];
+	} while(CanAddConnectionFrom(otherRoom) == 0 || isSameRoom(randomRoom, otherRoom) == 1 || ConnectionAlreadyExists(randomRoom, otherRoom) == 1);
+	
+	ConnectRoom(randomRoom, otherRoom);
+	ConnectRoom(otherRoom, randomRoom);
+}
+
+void populateConnections(struct Room rooms[PLAY_ROOMS]){
+	while (isGraphFull(rooms) == 0){
+		AddRandomConnection(rooms);
+	}
+}
+
+void writeToDisk(struct Room rooms[PLAY_ROOMS], char* main_directory){
+	chdir(main_directory);
+	int i;
+	for (i = 0; i < PLAY_ROOMS; i++){
+		FILE *fp = fopen(rooms[i].name, "w");
+		fprintf(fp, "ROOM NAME: %s\n", rooms[i].name);
+		
+		/*map array indexes to names*/
+		int j;
+		int found = 0;
+		for (j = 0; j < PLAY_ROOMS; j++){
+			int neighbor = rooms[i].neighbors[j];
+			if (neighbor){
+				found += 1;
+				fprintf(fp, "CONNECTION %d: %s\n", found, rooms[j].name);
+			}
+		}
+
+		/*stringify room_type enum*/
+		fprintf(fp, "ROOM TYPE: ");
+		if (rooms[i].type == START_ROOM){
+			fprintf(fp, "START_ROOM");
+		}
+		else if (rooms[i].type == END_ROOM){
+			fprintf(fp, "END_ROOM");
+		}
+		else {
+			fprintf(fp, "MID_ROOM");
+		}
+		
+		/*add a new line*/
+		fprintf(fp, "\n");
+
+		fclose(fp);
+	}
+}
+
+/* MAIN */
+
+int main(){
+	char *main_directory = createDir();
+	srand(time(NULL));
+	struct Room my_rooms[PLAY_ROOMS];
+	init_rooms(my_rooms);
+	populateConnections(my_rooms);
+	writeToDisk(my_rooms, main_directory);
+	free(main_directory);
+	return 0;
+}
